70b1e9e9f0f2d65a8635796a72e9bb606a079218
Dimitrijs Fedotovs <dima@fedoto.ws>
2017-03-16 22:22:31 +0200

    working with embedded database

--- src/main/java/lv/tele2/javaschool/phonebook/Record.java
+++ src/main/java/lv/tele2/javaschool/phonebook/Record.java
@@ -1,7 +1,6 @@
 package lv.tele2.javaschool.phonebook;
 
-import java.io.IOException;
-import java.io.Serializable;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,22 +10,12 @@ import java.util.List;
  * @version 1.0
  * @since 1.0
  */
-public class Record implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-    private static int nextId = 1;
-
+public class Record {
     private int id;
     private String name;
     private List<String> phoneList = new ArrayList<>();
 
-    public Record() {
-        this.id = nextId;
-        nextId++;
-    }
-
     public Record(String name, String... phones) {
-        this();
         this.name = name;
         Collections.addAll(this.phoneList, phones);
     }
@@ -47,14 +36,77 @@ public class Record implements Serializable {
         return phoneList;
     }
 
-    private void readObject(java.io.ObjectInputStream in)
-            throws IOException, ClassNotFoundException {
-        in.defaultReadObject();
-        nextId = Math.max(id + 1, nextId);
-    }
-
     @Override
     public String toString() {
         return String.format("%s %s %s", id, name, phoneList);
+    }
+
+    public void save() throws SQLException {
+        Connection con = Main.getDatabase().getConnection();
+        try (Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery("select max(id) from record")) {
+            rs.next();
+            int maxId = rs.getInt(1);
+            id = maxId + 1;
+        }
+        try (PreparedStatement stmtRec
+                     = con.prepareStatement("insert into record (id, name) values (?, ?)");
+             PreparedStatement stmtPhone
+                     = con.prepareStatement("insert into phone (record_id, phone) values (?, ?)")) {
+            stmtRec.setInt(1, id);
+            stmtRec.setString(2, name);
+            stmtRec.executeUpdate();
+
+            stmtPhone.setInt(1, id);
+
+            for (String p : phoneList) {
+                stmtPhone.setString(2, p);
+                stmtPhone.executeUpdate();
+            }
+        }
+    }
+
+    public static List<Record> findAll() throws SQLException {
+        List<Record> result = new ArrayList<>();
+        Connection con = Main.getDatabase().getConnection();
+        try (Statement stmt = con.createStatement();
+             ResultSet rs
+                     = stmt.executeQuery("select * from record")) {
+            while (rs.next()) {
+                Record r = construct(rs);
+                result.add(r);
+            }
+        }
+        return result;
+    }
+
+    private static Record construct(ResultSet rs) throws SQLException {
+        Connection con = Main.getDatabase().getConnection();
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        Record r = new Record(name);
+        r.id = id;
+        try (PreparedStatement stmt = con.prepareStatement("select * from phone where record_id = ?")) {
+            stmt.setInt(1, id);
+            try (ResultSet rsPhone = stmt.executeQuery()) {
+                while (rsPhone.next()) {
+                    String phone = rsPhone.getString("phone");
+                    r.phoneList.add(phone);
+                }
+            }
+        }
+        return r;
+    }
+
+    public static void remove(int id) throws SQLException {
+        Connection con = Main.getDatabase().getConnection();
+        try (PreparedStatement stmtPhone = con.prepareStatement("delete from phone where record_id = ?");
+             PreparedStatement stmtRec = con.prepareStatement("delete from record where id = ?")) {
+            stmtPhone.setInt(1, id);
+            stmtPhone.executeUpdate();
+            stmtRec.setInt(1, id);
+            stmtRec.executeUpdate();
+        }
+
     }
 }
